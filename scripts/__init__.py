1db6b5016a4346152f50edda835fe44ae256ba7b
Selam A. <selamsasnake@gmail.com>
2025-12-04 20:56:10 +0300

    chore: update scripts package

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,3 +3,6 @@ from scripts.preprocessor import *
 from scripts.plot import *
 from scripts.sentiment_analysis import *
 from scripts.data_loader import *
+from scripts.keyword_extractor import *
+from scripts.theme_extraction import *
+from scripts.thematic_analysis import *
